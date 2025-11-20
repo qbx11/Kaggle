b90b011d61c70ec914db9a43bec788b9536cb913
Kuba Janik <280170@student.pwr.edu.pl>
2025-11-20 02:02:44 +0100

    add new features

--- Titanic/src/data_preprocessing.py
+++ Titanic/src/data_preprocessing.py
@@ -2,15 +2,13 @@ import pandas as pd
 from pathlib import Path
 
 #file paths
-script_dir = Path(__file__).parent  # To jest src/
-project_dir = script_dir.parent      # To jest Titanic/
+current_dir = Path.cwd()
+project_dir = current_dir.parent
 train_path = project_dir / 'data' / 'raw' / 'test.csv'
 output_path = project_dir / 'data' / 'processed' / 'test_processed.csv'
 
 #read data
 train = pd.read_csv(train_path)
-
-
 
 #FEATURE ENGINNERING AND PREPROCESSING
 
@@ -51,8 +49,8 @@ train['Large family'] = (train['Family size']>3).astype(int)
 train["Embarked"] = train["Embarked"].transform(lambda x: x.fillna('S'))
 
 #one-hot encoding for 'Title' column
-title_dummies = pd.get_dummies(train['Title'],prefix='Title_').astype(int)
-train = pd.concat([train,title_dummies],axis=1)
+title_dummies = pd.get_dummies(train['Title'], prefix='Title_').astype(int)
+train = pd.concat([train,title_dummies], axis=1)
 
 #one-hot encoding for 'Embarked' column
 embarked_dummies = pd.get_dummies(train['Embarked'],prefix='Embarked_').astype(int)
@@ -74,7 +72,6 @@ train['AgeGroup'] = train['Age'].apply(categorize_age)
 age_dummies = pd.get_dummies(train['AgeGroup'], prefix='Age').astype(int)
 train = pd.concat([train,age_dummies],axis=1)
 
-
 #new feature: FarePerPerson
 train['Fare'] = train['Fare'].fillna(train['Fare'].median())
 train['FarePerPerson'] = (train['Fare'] / (train['SibSp'] + train['Parch'] + 1)).round(2)
@@ -82,6 +79,18 @@ train['FarePerPerson'] = (train['Fare'] / (train['SibSp'] + train['Parch'] + 1))
 #new feature: HasCabin
 train['HasCabin'] = train['Cabin'].notna().astype(int)
 
+#new feature: Deck number
+train['Deck'] = train['Cabin'].str[0]
+train['Deck'] = train['Deck'].fillna('Unknown')
+
+deck_dummies = pd.get_dummies(train['Deck'],prefix='Deck').astype(int)
+train = pd.concat([train,deck_dummies],axis=1)
+
+#new feature: isFemaleFirstClass (highest chance of survival)
+train['isFemaleFirstClass'] = ((train['Sex_numeric']==1) & (train['Pclass']==1)).astype(int)
+
+#new featyre: isMaleThirdClass (lowest chance of survival)
+train['isMaleThirdClass'] = ((train['Sex_numeric']==0) & (train['Pclass']==3)).astype(int)
 
 
 #processed data frame to csv
